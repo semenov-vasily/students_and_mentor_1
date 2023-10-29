fa9cd2beceb7af7ce925ff2ef4de69bd83c3972f
Semenov Vasily <vvs1771@gmail.com>
2023-10-29 19:04:20 +0300

    commit_5

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades_student = {}
-        self.grades_average_homework = []
+
+
     def rate_hw_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades_lecturer:
@@ -15,7 +16,9 @@ class Student:
                 lecturer.grades_lecturer[course] = [grade]
         else:
             return 'Ошибка'
-    def grades_average_homework(self):
+
+
+    def grades_average(self):
         grades_count = 0
         grades_sum = 0
         for grade in self.grades_student:
@@ -27,11 +30,10 @@ class Student:
                 return 0
 
 
-
     def __str__(self):
         return (f"Имя: {self.name}\n"
                 f"Фамилия: {self.surname}\n"
-                f"Средняя оценка за домашние задания: {self.grades_average_homework}\n"
+                f"Средняя оценка за домашние задания: {self.grades_average()}\n"
                 f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
                 f"Завершенные курсы: {', '.join(self.finished_courses)}\n")
 
@@ -42,21 +44,36 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
         self.grades_lecturer = {}
-        self.grades_average_lecture = []
+
+
+    def grades_average(self):
+        grades_count = 0
+        grades_sum = 0
+        for grade in self.grades_lecturer:
+            grades_count += len(self.grades_lecturer[grade])
+            grades_sum += sum(self.grades_lecturer[grade])
+            if grades_count > 0:
+                return grades_sum / grades_count
+            else:
+                return 0
+
+
     def __str__(self):
         return (f"Имя: {self.name}\n"
                 f"Фамилия: {self.surname}\n"
-                f"Средняя оценка за лекции: {', '.join(self.grades_average_lecture)}\n")
+                f"Средняя оценка за лекции: {self.grades_average()}\n")
 
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
 
     def rate_hw_student(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -67,9 +84,11 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+
     def __str__(self):
         return (f"Имя: {self.name}\n"
                 f"Фамилия: {self.surname}\n")
+
 
 
 student_1 = Student('Ruoy', 'Eman', 'your_gender')
@@ -103,7 +122,5 @@ print(mentor_reviewer_1)
 print(mentor_lecturer_2)
 print(student_1)
 
-# student_1.grades_average_homework()
-# print(student_1)
 
 print('ok')
