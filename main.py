94b7855585f794f0b6dd3910d91d61b01356be40
Semenov Vasily <vvs1771@gmail.com>
2023-10-30 01:21:29 +0300

    commit_7

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades_student = {}
+        self.student = []
 
 
     def rate_hw_lecturer(self, lecturer, course, grade):
@@ -17,20 +18,36 @@ class Student:
         else:
             return 'Ошибка'
 
-
     def grades_average(self):
         grades_count = 0
         grades_sum = 0
         for grade in self.grades_student:
             grades_count += len(self.grades_student[grade])
             grades_sum += sum(self.grades_student[grade])
-            if grades_count > 0:
-                return grades_sum / grades_count
-            else:
-                return 0
+        if grades_count > 0:
+            return grades_sum / grades_count
+        else:
+            return 0
 
+
+    def __gt__(self, other):
+        if not isinstance(other, Student):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() > other.grades_average()
     def __lt__(self, other):
-        pass
+        if not isinstance(other, Student):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() < other.grades_average()
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() == other.grades_average()
+
+
+
 
 
 
@@ -62,11 +79,26 @@ class Lecturer(Mentor):
         for grade in self.grades_lecturer:
             grades_count += len(self.grades_lecturer[grade])
             grades_sum += sum(self.grades_lecturer[grade])
-            if grades_count > 0:
-                return grades_sum / grades_count
-            else:
-                return 0
+        if grades_count > 0:
+            return grades_sum / grades_count
+        else:
+            return 0
 
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() > other.grades_average()
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() < other.grades_average()
+    def __eq__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Такое сравнение некорректно')
+            return
+        return self.grades_average() == other.grades_average()
 
     def __str__(self):
         return (f"Имя: {self.name}\n"
@@ -98,33 +130,81 @@ class Reviewer(Mentor):
 student_1 = Student('Ruoy', 'Eman', 'your_gender')
 student_1.courses_in_progress += ['Python']
 student_1.courses_in_progress += ['Git']
+student_1.courses_in_progress += ['Django']
 student_1.finished_courses += ['Введение в программирование']
 
+student_2 = Student('Tom', 'Cruise', 'your_gender')
+student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
+student_2.courses_in_progress += ['Django']
+student_2.finished_courses += ['Введение в программирование']
 
 mentor_reviewer_1 = Reviewer('Some', 'Buddy')
 mentor_reviewer_1.courses_attached += ['Python']
 mentor_reviewer_1.courses_attached += ['Git']
+mentor_reviewer_1.courses_attached += ['Django']
 
-mentor_lecturer_2 = Lecturer('Lom', 'Pud')
+mentor_lecturer_1 = Lecturer('Lom', 'Pud')
+mentor_lecturer_1.courses_attached += ['Python']
+mentor_lecturer_1.courses_attached += ['Git']
+mentor_lecturer_1.courses_attached += ['Django']
+
+mentor_lecturer_2 = Lecturer('Katie', 'Holmes')
 mentor_lecturer_2.courses_attached += ['Python']
+mentor_lecturer_2.courses_attached += ['Git']
+mentor_lecturer_2.courses_attached += ['Django']
 
 mentor_reviewer_1.rate_hw_student(student_1, 'Python', 10)
 mentor_reviewer_1.rate_hw_student(student_1, 'Python', 10)
 mentor_reviewer_1.rate_hw_student(student_1, 'Python', 8)
-mentor_reviewer_1.rate_hw_student(student_1, 'Git', 8)
+mentor_reviewer_1.rate_hw_student(student_1, 'Git', 10)
+mentor_reviewer_1.rate_hw_student(student_1, 'Django', 8)
 
-student_1.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
-student_1.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
-student_1.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Python', 8)
+mentor_reviewer_1.rate_hw_student(student_2, 'Git', 10)
+mentor_reviewer_1.rate_hw_student(student_2, 'Django', 8)
 
-print(student_1.grades_student)
-print(student_1.courses_in_progress)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 10)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 10)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Python', 7)
+student_1.rate_hw_lecturer(mentor_lecturer_1, 'Git', 8)
 
-print(mentor_lecturer_2.grades_lecturer)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 10)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Python', 7)
+student_2.rate_hw_lecturer(mentor_lecturer_2, 'Git', 8)
+
+print(f'Оценки студента {student_1.name} {student_1.surname}: ', student_1.grades_student)
+print(f'Оценки студента {student_2.name} {student_2.surname}: ',student_2.grades_student)
+print(f'Курсы студента {student_1.name} {student_1.surname}: ',student_1.courses_in_progress)
+print(f'Курсы студента {student_2.name} {student_2.surname}: ',student_2.courses_in_progress)
+
+print(f'Оценки лектора {mentor_lecturer_1.name} {mentor_lecturer_1.surname}: ',mentor_lecturer_1.grades_lecturer)
+print(f'Оценки лектора {mentor_lecturer_2.name} {mentor_lecturer_2.surname}: ',mentor_lecturer_2.grades_lecturer)
 
 print(mentor_reviewer_1)
+print(mentor_lecturer_1)
 print(mentor_lecturer_2)
 print(student_1)
+print(student_2)
+
+if student_1 > student_2:
+    print(f'Средняя оценка {student_1.name} {student_1.surname} больше, чем средняя оценка {student_2.name} {student_2.surname}')
+elif student_1 < student_2:
+    print(f'Средняя оценка {student_1.name} {student_1.surname} меньше, чем средняя оценка {student_2.name} {student_2.surname}')
+else:
+    print(f'Средняя оценка {student_1.name} {student_1.surname}  равна средней оценке {student_2.name} {student_2.surname}')
+
+
+if mentor_lecturer_1 > mentor_lecturer_2:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname} больше, чем средняя оценка {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+elif student_1 < student_2:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname} меньше, чем средняя оценка {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+else:
+    print(f'Средняя оценка {mentor_lecturer_1.name} {mentor_lecturer_1.surname}  равна средней оценке {mentor_lecturer_2.name} {mentor_lecturer_2.surname}')
+
 
 
 print('ok')
